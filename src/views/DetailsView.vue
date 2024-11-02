cbb8becc8f53d14fb390d0a40cde05640a2bf8b1
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2024-11-02 12:47:27 +0100

    app init

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Details 
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <p class="text-3xl">Home page</p>
+  </main>
+</template>
